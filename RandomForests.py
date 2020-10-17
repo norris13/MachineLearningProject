1852d3d2dca4ab6339570e3c33062fc58b871213
Jamahl Savage <47194466+jrsavage29@users.noreply.github.com>
2020-10-17 15:44:21 -0400

    Version 1 of Random Forests Algorithm
    Still needs code for reading in either a csv or npy

---
+++ RandomForests.py
@@ -0,0 +1,30 @@
+
+#Use matplotlib for plots
+import matplotlib.pyplot as plt
+# Use numpy to convert to arrays
+import numpy as np
+# Import the model we are using
+from sklearn.ensemble import RandomForestRegressor
+# Using Skicit-learn to split data into training and testing sets
+from sklearn.model_selection import train_test_split
+
+features =
+labels = 
+
+# Split the data into training and testing sets
+train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
+
+print('Training Features Shape:', train_features.shape)
+print('Training Labels Shape:', train_labels.shape)
+print('Testing Features Shape:', test_features.shape)
+print('Testing Labels Shape:', test_labels.shape)
+
+# Instantiate model with 1000 decision trees
+rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+# Train the model on training data
+rf.fit(train_features, train_labels);
+
+# Use the forest's predict method on the test data
+predictions = rf.predict(test_features)
+# Calculate the absolute errors
+errors = abs(predictions - test_labels)
