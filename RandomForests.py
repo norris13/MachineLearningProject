36689d995788d757d0065e1eda00d40a7b2e8440
Jamahl Savage <47194466+jrsavage29@users.noreply.github.com>
2020-10-22 12:30:20 -0400

    Updated for Csv file names

--- RandomForests.py
+++ RandomForests.py
@@ -10,12 +10,12 @@ from sklearn.ensemble import RandomForestRegressor
 # Using Skicit-learn to split data into training and testing sets
 from sklearn.model_selection import train_test_split
 
-data_orig = pd.read_csv("football_data.csv")
+data_orig = pd.read_csv("teamElos.csv")
 data = data_orig.copy()
 
 print(data.columns)
-features = data.drop('Feature', axis=1)
-labels = data['Feature']
+features = data.drop('Offensive Elo Rating', axis=1)
+labels = data['Offensive Elo Rating']
 
 # Split the data into training and testing sets
 train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
@@ -28,7 +28,7 @@ print('Testing Labels Shape:', test_labels.shape)
 # Instantiate model with 1000 decision trees
 rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
 # Train the model on training data
-rf.fit(train_features, train_labels);
+rf.fit(train_features, train_labels)
 
 # Use the forest's predict method on the test data
 predictions = rf.predict(test_features)
