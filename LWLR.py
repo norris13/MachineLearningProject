4a8e5d5fe416e52f776b58ad30ba0568fc0a1821
IndieAddict <platoaether@gmail.com>
2020-12-01 17:37:44 -0500

    Updated to begin code

--- LWLR.py
+++ LWLR.py
@@ -1,1 +1,13 @@
+import requests, zipfile, io
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Load csv
+data_orig = pd.read_csv("allSeasonScores.csv")
+
+'''
+jsonObject
+jsonObj[teamAbbr][year][week][unit]
+where unit is either 'off' or 'def'
+'''
