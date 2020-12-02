b6c3901bc45ff0d400459e2f6fb38a2565646aa1
IndieAddict <alexander.grant.ryan@gmail.com>
2020-12-02 13:25:23 -0500

    Added json loading

--- LWLR.py
+++ LWLR.py
@@ -1,7 +1,13 @@
-import requests, zipfile, io
+import requests, zipfile, io, json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+# Load elo rankings
+with open("eloVals.json") as file:
+    elos = json.load(file)
+
 
 # Load csv
 data_orig = pd.read_csv("allSeasonScores.csv")
