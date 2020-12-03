1465df5fbdaaf865c439596471ec3cd5f401855f
Jamahl Savage <47194466+jrsavage29@users.noreply.github.com>
2020-12-02 22:54:00 -0500

    Updated to read in data from allSeasonsScores.csv
    Outputs the mse, rmse, and mae values at the end of the model test

--- RandomForests.py
+++ RandomForests.py
@@ -1,4 +1,5 @@
-
+import json
+import math
 #Use pandas for messing with the csv
 import pandas as pd
 #Use matplotlib for plots
@@ -7,15 +8,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 # Import the model we are using
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 # Using Skicit-learn to split data into training and testing sets
 from sklearn.model_selection import train_test_split
 
-data_orig = pd.read_csv("teamElos.csv")
+#jsonObj[teamAbbr][year][week][unit]
+#where unit is either 'off' or 'def'
+
+with open("eloVals.json") as file:
+    elos = json.load(file)
+
+data_orig = pd.read_csv("./data/allSeasonScores.csv")
 data = data_orig.copy()
 
-print(data.columns)
-features = data.drop('Offensive Elo Rating', axis=1)
-target = data['Offensive Elo Rating']
+#print(data)
+home_off_elo = []
+home_def_elo = []
+away_off_elo = []
+away_def_elo = []
+
+# for column in data: 
+#     # Select column contents by column 
+#     # name using [] operator 
+#     columnSeriesObj = data[column] 
+#     print('Colunm Name : ', column) 
+#     print('Column Contents : ', columnSeriesObj.values) 
+
+      
+# Select column contents by column 
+# name using [] operator 
+homeTeamNames = data[data.columns[3]]
+# print('Column Name : ', data.columns[3]) 
+# print('Column Contents : ', homeTeamNames.values)
+
+awayTeamNames = data[data.columns[4]]
+
+year = data[data.columns[1]]
+
+week = data[data.columns[2]]
+
+#choose either off or def for unit
+
+num_rows = data.shape[0]
+
+#Create lists of elos so they can be appended to the dataFrame
+for i in range(num_rows):
+    home_off_elo.insert(i, elos[ homeTeamNames[i] ][ str(year[i]) ][ str(week[i]) ]['off'])
+    home_def_elo.insert(i, elos[ homeTeamNames[i] ][ str(year[i]) ][ str(week[i]) ]['def'])
+    away_off_elo.insert(i, elos[ awayTeamNames[i] ][ str(year[i]) ][ str(week[i]) ]['off'])
+    away_def_elo.insert(i, elos[ awayTeamNames[i] ][ str(year[i]) ][ str(week[i]) ]['def'])
+
+elo_data = data.assign(homeOffElo = home_off_elo, awayOffElo = away_off_elo, homeDefElo = home_def_elo, awayDefElo = away_def_elo)
+print(elo_data)
+
+features = elo_data.drop( ['homeOffPoints', 'count', 'homeTeam', 'awayTeam'], axis=1 )
+
+target = elo_data['homeOffPoints']
 
 feature_list = list(features.columns)
 
@@ -23,13 +71,17 @@ feature_list = list(features.columns)
 features  = np.array(features)
 labels = np.array(target)
 
+
 # Split the data into training and testing sets
 train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
 
-print('Training Features Shape:', train_features.shape)
-print('Training Labels Shape:', train_labels.shape)
-print('Testing Features Shape:', test_features.shape)
-print('Testing Labels Shape:', test_labels.shape)
+# print('Training Features Shape:', train_features.shape)
+# print('Training Labels Shape:', train_labels.shape)
+# print('Testing Features Shape:', test_features.shape)
+# print('Testing Labels Shape:', test_labels.shape)
+
+# print(train_features)
+# print(train_labels)
 
 # Instantiate model with 1000 decision trees
 rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
@@ -38,14 +90,14 @@ rf.fit(train_features, train_labels)
 
 # Use the forest's predict method on the test data
 predictions = rf.predict(test_features)
-# Calculate the absolute errors
-errors = abs(predictions - test_labels)
 
-# Print out the mean absolute error (mae)
-print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+from sklearn import metrics
 
-# Calculate mean absolute percentage error (MAPE)
-mape = 100 * (errors / test_labels)
-# Calculate and display accuracy
-accuracy = 100 - np.mean(mape)
-print('Accuracy:', round(accuracy, 2), '%.')
+print('Mean Absolute Error:', metrics.mean_absolute_error(test_labels, predictions))
+print('Mean Squared Error:', metrics.mean_squared_error(test_labels, predictions))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(test_labels, predictions)))
+
+#mse = mean_squared_error(test_labels, predictions)
+#rmse = math.sqrt(mse)
+#print('Accuracy for Random Forest',100*max(0,rmse)) 
+#print('Accuracy for Random Forest',max(0,mse)) 
